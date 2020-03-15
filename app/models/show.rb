6f1b5115ac13f89254afd4f113f1f44492e52d5f
dwane richards <dwanerich@gmail.com>
2020-03-15 15:48:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   
-  def actors_list
+  def self.actors_list
     # full names of each actor associated with show
     actors.map do |t|
       t.actors
