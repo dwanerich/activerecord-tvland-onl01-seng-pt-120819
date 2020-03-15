31f699fd16cb28bf372cdaf0a9fb3282a2af889c
dwane richards <dwanerich@gmail.com>
2020-03-15 15:54:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   
   def actors_list
     self.actors do |t|
-     return #{first_name} + " " #{last_name}
+     return t first_name + " " last_name
     end
   end
   
