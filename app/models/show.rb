6f7fa91a476260f96a7372625b32f4154933de6d
dwane richards <dwanerich@gmail.com>
2020-03-15 15:38:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,10 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   def actors_list
-    
+    # full names of each actor associated with show
+    self.actors do |t|
+      t.actors
+    end
   end
   
   
